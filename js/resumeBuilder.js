4316a4ad45dd31e4cfadcc70263afa4e94713685
Matthew Nerger <mnerger@lsu.edu>
2016-03-01 17:01:50 -0600

    Upload progress to Github (far too late, having forgotten to do so all week)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,151 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//Objects
+
+//bio object
+var bio = {
+	"name" : "Matthew Nerger",
+	"role" : "Front-End Developer",
+	"contacts" : {
+		"mobile" : "813.758.8512",
+		"email" : "mnerger@lsu.edu",
+		"github" : "towercity",
+		"twitter" : "towercitydrive",
+		"location" : "Baton Rouge, LA"
+	},
+	"bioPic" : "images/fry.jpg",
+	"welcomeMessage" : "Hello, friend!",
+	"skills" : [
+		"writing", "programming", "teaching", "JS"
+	]
+};
+
+//work object
+var work = {
+	"jobs" : [
+		{
+		"employer" : "Franciscan Ministries of Our Lady Health System",
+		"title" : "AP Clerk",
+		"location" : "Baton Rouge, LA",
+		"dates" : "December 2014 - Present",
+		"description" : "Processed invoices"
+		},
+		{
+		"employer" : "LSU Cox Center for Student Athletes",
+		"title" : "Content Tutor",
+		"location" : "Baton Rouge, LA",
+		"dates" : "July - December 2014",
+		"description" : "Taught English, writing"
+		},
+		{
+		"employer" : "WFSU-FM",
+		"title" : "Production Assisstant",
+		"location" : "Tallahassee, FL",
+		"dates" : "January - July 2014",
+		"description" : "Ran automation software, maintained databases"
+		},
+		{
+		"employer" : "WVFS Tallahassee",
+		"title" : "Disc Jockey",
+		"location" : "Tallahassee, FL",
+		"dates" : "January 2012 - December 2013",
+		"description" : "Reviewed incoming music, maintained music catalog, ran all on-air audio"
+		}
+	]
+};
+
+//education object
+var education = {
+	"schools" : [
+		{
+		"name" : "Florida State University",
+		"city" : "Tallahassee, FL",
+		"major" : "Creative Writing",
+		"minor" : "Film Studies",
+		"graduation" : 2013,
+		"years" : 4
+		}
+	],
+	"onineCourses" : [
+		{
+		"title"  : "Front-End Developer",
+		"school" : "Udacity",
+		"dates" : 2016,
+		"url" : "https://www.udacity.com/"
+		}
+	]
+};
+
+//project object
+var projects = {
+	"projects" : [
+	{
+		"title" : "Work Portfolio",
+		"dates" : 2016,
+		"description" : "Portfolio of audio experience"
+	}
+	]
+};
+
+//Functions
+
+//Prints Work Exp to the page
+work.display = function() {
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+		$(".work-entry:last").append(
+			HTMLworkEmployer.replace("%data%", work.jobs[job].employer) +
+			HTMLworkTitle.replace("%data%", work.jobs[job].title) +
+			HTMLworkLocation.replace("%data%", work.jobs[job].location) +
+			HTMLworkDates.replace("%data%", work.jobs[job].dates) +
+			HTMLworkDescription.replace("%data%", work.jobs[job].description)
+		);
+	};
+}
+
+//Prints Projects to the Page
+projects.display = function() {
+	for (project in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+		$(".project-entry:last").append(
+			HTMLprojectTitle.replace("%data%", projects.projects[project].title) +
+			HTMLprojectDates.replace("%data%", projects.projects[project].dates) +
+			HTMLprojectDescription.replace("%data%", projects.projects[project].description) +
+			HTMLprojectImage.replace("%data%", projects.projects[project].image)
+		);
+	}
+};
+
+
+//formatted variables
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedSkills = [HTMLskills.replace("%data%", bio.skills[0]), HTMLskills.replace("%data%", bio.skills[1]), HTMLskills.replace("%data%", bio.skills[2]), HTMLskills.replace("%data%", bio.skills[3])];
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+
+//Page Display
+
+//header 
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedMobile);
+$("#header").append(formattedEmail);
+$("#header").append(formattedGithub);
+$("#header").append(formattedTwitter);
+$("#header").append(formattedLocation);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
+//checks for skills before printing skills header
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+};
+$("#header").append(formattedSkills);
+
+//Function calls
+work.display();
+projects.display();
